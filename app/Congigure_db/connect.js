ca75cd63f34f4a6e034d3827f06a676a6176f3ca
Parul Bansal <bparul999@gmail.com>
2020-04-26 13:01:49 +0530

    Folder name changed

--- app/Congigure_db/connect.js
+++ app/Congigure_db/connect.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Gaurav@parul7',
-    database: 'user'
+    database: 'Login'
 });
 
 connection.connect(function(err) {
